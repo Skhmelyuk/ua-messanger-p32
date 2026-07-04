60643059ca782dcded2e60ed61d5248348982d42
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-07-04 21:42:54 +0300

    lessomn-03

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,6 @@ import { styles } from "@/styles/feed.styles";
 import { useAuthActions } from "@convex-dev/auth/react";
 import { Ionicons } from "@expo/vector-icons";
 import { useQuery } from "convex/react";
-import { StoriesSection } from "@/components/StoriesSection";
 import {
   ActivityIndicator,
   FlatList,
